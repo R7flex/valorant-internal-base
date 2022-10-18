bc33c04e1106920f41dd835f268e52bcd19f9a58
R7flex <50244396+R7flex@users.noreply.github.com>
2022-10-18 22:07:11 +0300

    Update offsets.h

--- internal for ugly pasters/offsets.h
+++ internal for ugly pasters/offsets.h
@@ -2,9 +2,9 @@
 namespace Offsets
 {
 	constexpr auto
-		Key = 0x9261938,
-		State = 0x9261900,
-		ProcessEvent = 0x3510670,
-		StaticFindObject = 0x3551110,
-		TriggerVEH = 0x1D97FE0;
+		Key = 0x9282978,
+		State = 0x9282940,
+		ProcessEvent = 0x3520430,
+		StaticFindObject = 0x35610e0,
+		TriggerVEH = 0x1D9DC50;
 }
